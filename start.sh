89099cdc386f366e3bbdbaea98fed0d7b343d35c
praveen28624 <68008375+praveen28624@users.noreply.github.com>
2022-10-14 18:48:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/praveen28624/oracle /oracle
+git clone xxxxxxx /oracle
 cd /oracle
 echo "Starting Bot, Please Wait..."
 python3 bot.py
